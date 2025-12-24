e09931f1319597ba64b61151c604e83eeffcb8b2
Sujay Sen <163733398+sujay543@users.noreply.github.com>
2025-12-25 00:10:03 +0530

    Add files via upload

---
+++ Array/mountainarray.java
@@ -0,0 +1,21 @@
+public class mountainarray {
+    public static void main(String args[]){
+        int[] arr = {1,2,3,6,5,4};
+        System.out.println(peakIndexInMountainArray(arr));
+    }
+
+    public static int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length-1;
+        while(start < end){
+            int mid = start + (end - start)/2;
+            if(arr[mid] < arr[mid + 1]){
+                start = mid + 1;
+            }else{
+                end = mid;
+            }
+        }
+        return start;
+    
+}
+}
