56c90a1db24ed942ee0d1183f80675b2929d6cdc
Sujay Sen <163733398+sujay543@users.noreply.github.com>
2026-04-12 16:17:20 +0530

    implement finding first and last position of an element using binary search

--- BinarySearch/firstandlastPosition.java
+++ BinarySearch/firstandlastPosition.java
@@ -1,9 +1,50 @@
 package BinarySearch;
 
+import java.util.Arrays;
+
 public class firstandlastPosition {
     public static void main(String args[]) {
         int[] nums = { 5, 7, 7, 8, 8, 10 };
         int target = 8;
+        System.out.println(Arrays.toString(search(nums, target)));
+    }
 
+    public static int[] search(int[] arr, int target) {
+        return new int[] { searchfirstPosition(arr, target), searchLastPosition(arr, target) };
+
+    }
+
+    static int searchfirstPosition(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        int ans = -1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] >= target) {
+                if (arr[mid] == target) {
+                    ans = mid; // store answer
+                }
+                end = mid - 1; // move left
+            } else {
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    static int searchLastPosition(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] <= target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return end;
     }
 }
