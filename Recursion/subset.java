01134bbc64d209395450a63471701d58ef7fcb00
Sujay Sen <163733398+sujay543@users.noreply.github.com>
2026-04-15 12:57:34 +0530

    implement suset to find all the possible subarray of an array

---
+++ Recursion/subset.java
@@ -0,0 +1,30 @@
+package Recursion;
+
+import java.util.ArrayList;
+
+import java.util.List;
+
+public class subset {
+    public static void main(String args[]) {
+        int[] arr = { 1, 2, 3 };
+        System.out.println(subsets(arr));
+    }
+
+    static List<List<Integer>> subsets(int[] arr) {
+        List<List<Integer>> result = new ArrayList<>();
+        helper(arr, new ArrayList<>(), result, 0);
+        return result;
+    }
+
+    static void helper(int[] arr, List<Integer> current, List<List<Integer>> result, int i) {
+        if (i == arr.length) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+        current.add(arr[i]);
+        helper(arr, current, result, i + 1);
+        current.remove(current.size() - 1);
+        helper(arr, current, result, i + 1);
+
+    }
+}
